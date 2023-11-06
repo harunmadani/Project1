af5b26eae7021a0f035458f936feeed9f2ef8472
kellerneu <146773477+kellerneu@users.noreply.github.com>
2023-11-05 22:08:12 -0600

    Add files via upload

--- kwn0018project2.cpp
+++ kwn0018project2.cpp
@@ -14,14 +14,13 @@ using namespace std;
     */
 
    //part 2
-   const int row = 5;
+   const int row = 5; //declaring global constants for array sizes
    const int col = 5;
-   //const int size = 2;
-   enum option {displayLeft = '1', displayRight = '2', guess = '3', reset = '4', exitGame = '5'};
+   enum option {displayLeft = '1', displayRight = '2', guess = '3', reset = '4', exitGame = '5'}; //enumerated values for switch case
 
 
     //part 3
-   string getName(string userName)
+   string getName(string userName) //function for username, will return a string
     {
         
         bool checkAlpha = false;
@@ -54,7 +53,7 @@ using namespace std;
     }
 
     //part 4
-    int genShowMatrix(int visArray[][col])
+    int genShowMatrix(int visArray[][col]) //generates displayed matrix, every value is -1
     {
         for(int i=0; i<row; ++i)
         {
@@ -67,7 +66,7 @@ using namespace std;
     }
 
     //part 5
-    int genHideMatrix(int hiddenArray[][col], int randnumArray[], int size)
+    int genHideMatrix(int hiddenArray[][col], int randnumArray[], int size) //generates hidden matrix
     {
         for(int i=0; i<row; i++)
         {
@@ -75,16 +74,16 @@ using namespace std;
             for(int j=0; j<col; j++)
             {
                 do{
-                hiddenArray[i][j] = (rand() % 200) + 100;
+                hiddenArray[i][j] = (rand() % 200) + 100; //loop generates numbers in between the two random numbers and stores into array
                 }while(hiddenArray[i][j] < randnumArray[0] || hiddenArray[i][j] > randnumArray[1]);
             }
         }
         
-        return hiddenArray[row][col];
+        return hiddenArray[row][col]; //returns values into the hidden array
     }
 
     //part 6
-    void showMatrix(int visArray[][col])
+    void showMatrix(int visArray[][col]) //displays visual matrix
     {
         for(int i=0; i<row; ++i)
         {
@@ -101,7 +100,7 @@ using namespace std;
     //part 7
     int setdisplayLeft(int hiddenArray[][col], int visArray[][col], int randnumArray[], int size, int int1, int int2)
     {
-        int1 = randnumArray[0];
+        int1 = randnumArray[0]; //int1 is showed as the lowerbound number
             cout << "A correct guess will give you 1 point, and an incorrect guess will take 10 points away." << endl << endl;
             cout << int1 << "\t" << int2 << endl << endl;
             cout << "1. Display left number" << endl;
@@ -111,13 +110,13 @@ using namespace std;
             cout << "5. Exit" << endl;
             cout << "What is your option?: ";
 
-        return int1;
+        return int1; //updates int1 in main
     }
 
     //part 8
     int setdisplayRight(int hiddenArray[][col], int visArray[][col], int randnumArray[], int size, int int1, int int2)
     {
-        int2 = randnumArray[1];
+        int2 = randnumArray[1]; //int2 is showed as the upperbound number
             cout << "A correct guess will give you 1 point, and an incorrect guess will take 10 points away." << endl << endl;
             cout << int1 << "\t" << int2 << endl << endl;
             cout << "1. Display left number" << endl;
@@ -127,11 +126,11 @@ using namespace std;
             cout << "5. Exit" << endl;
             cout << "What is your option?: ";
             
-        return int2;
+        return int2; //updates int2 in main
     }
 
     //part 9
-    int eliminate(int hiddenArray[][col], int visArray[][col], int rowIndex, int colIndex)
+    int eliminate(int hiddenArray[][col], int visArray[][col], int rowIndex, int colIndex) //for changing -1 values in visual display to 0
     {
         for(int i=0; i<row; ++i)
         {
@@ -156,7 +155,7 @@ using namespace std;
     }
 
     //part 10
-    bool allZeros(int visArray[][col], bool isZero)
+    bool allZeros(int visArray[][col], bool isZero) //checks if the visual array values are all 0
     {
         for(int i=0; i<row; ++i)
         {
@@ -173,14 +172,14 @@ using namespace std;
                 }
             }
         }
-        return isZero;
+        return isZero; //returns the bool value
     }
 
     //part 11
-    int funcGuess(int userGuess, int hiddenArray[][col], int visArray[][col], int userPoints, int rowIndex, int colIndex, int int1, int int2, int randnumArray[])
+    int funcGuess(int userGuess, int hiddenArray[][col], int visArray[][col], int userPoints, int rowIndex, int colIndex, int int1, int int2, int randnumArray[]) //for the user's guess
     {
 
-        showMatrix(visArray);
+        showMatrix(visArray); //displays visual matrix
 
         int correctGuess = 0;
         cout << "Enter a guess: ";
@@ -190,30 +189,30 @@ using namespace std;
         {
             for(int j=0; j<col; ++j)
             {
-                if(userGuess == hiddenArray[i][j])
+                if(userGuess == hiddenArray[i][j]) //if the correct guess is one of the random numbers in between bounds
                 {
-                    correctGuess = hiddenArray[i][j];
-                    rowIndex = i;
+                    correctGuess = hiddenArray[i][j]; 
+                    rowIndex = i; //index is used for changing values to 0
                     colIndex = j;
                 }
             }
         }
-        if(userGuess == correctGuess)
+        if(userGuess == correctGuess) //if the user's guess is correct
         {
-            if(int1 == randnumArray[0] || int2 == randnumArray[1])
+            if(int1 == randnumArray[0] || int2 == randnumArray[1]) //if user has showed one of the bounds
             {
                 cout << "You guessed correctly, you get 1 point!" << endl;
                 userPoints += 1;
             }
-            if(int1 != randnumArray[0] && int2 != randnumArray[1])
+            if(int1 != randnumArray[0] && int2 != randnumArray[1]) //if user hasn't showed one of the bounds
             {
                 cout << "You guessed correctly, you get 5 points!" << endl;
                 userPoints += 5;
             }
 
-            visArray[row][col] = eliminate(hiddenArray, visArray, rowIndex, colIndex);
+            visArray[row][col] = eliminate(hiddenArray, visArray, rowIndex, colIndex); //calls eliminate function into visArray
 
-            showMatrix(visArray);
+            showMatrix(visArray); //displays visArray
 
             cout << endl;
 
@@ -226,7 +225,7 @@ using namespace std;
             cout << "5. Exit" << endl;
             cout << "What is your option?: ";
         }
-        if(userGuess != correctGuess && userPoints >= 0)
+        if(userGuess != correctGuess && userPoints >= 0) //if user guesses incorrectly
         {
             if(int1 == randnumArray[0] || int2 == randnumArray[1] && userPoints >= 0)
             {
@@ -264,21 +263,21 @@ using namespace std;
             }
         }
 
-        return userPoints;
+        return userPoints; //updates points to main
     }
 
     //part 12
-    int initialize(int randnumArray[], int visArray[][col], int hiddenArray[][col], int size)
+    int initialize(int randnumArray[], int visArray[][col], int hiddenArray[][col], int size) //initializes the game
     {
 
         srand(time(NULL));
         randnumArray[0] = (rand() % 100) + 100;  //generates the random numbers
         randnumArray[1] = (rand() % 100) + 200;
-        visArray[row][col] = genShowMatrix(visArray);
+        visArray[row][col] = genShowMatrix(visArray); //calls genShowMatrix into visArray
 
-        hiddenArray[row][col] = genHideMatrix(hiddenArray, randnumArray, size);
+        hiddenArray[row][col] = genHideMatrix(hiddenArray, randnumArray, size); //calls genHideMatrix into hiddenArray
 
-        return randnumArray[size];
+        return randnumArray[size]; //returns lower and upper bounds into main
     }
 
 
@@ -299,7 +298,7 @@ int main() {
     int int2 = -1;
 
     //part 15
-    const int size = 2;
+    const int size = 2; //declaring variables
     bool isZero;
     int rowIndex;
     int colIndex;
@@ -308,10 +307,10 @@ int main() {
     int hiddenArray[row][col];
     srand(time(NULL));
 
-    randnumArray[size] = initialize(randnumArray, visArray, hiddenArray, size);
+    randnumArray[size] = initialize(randnumArray, visArray, hiddenArray, size); //calling the initialize function to start the game
 
     //part 16
-    string userName = getName(userName);
+    string userName = getName(userName); //calls getName function to put userName into the string
 
     //part 17
     
@@ -332,17 +331,6 @@ int main() {
     if(userPoints >= 0){ //if user's points is less than 0, the game will end
     do{
 
-        if(userPoints < 0){ //ends the game if user points are less than 0
-                cout << endl << "Your total points balance: " << userPoints << endl;
-                cout << "You have run out of points." << endl;
-                cout << "Goodbye " << userName << "!" << endl;
-                userChoice = true;
-                break;
-            }
-
-        //do{
-
-    
     int choice;
     cin >> choice;
     
@@ -350,13 +338,10 @@ int main() {
     switch(choice) {
         case 1:
         
-        //userChoice = true;
+        userChoice = true;
         
         if(int2 == randnumArray[1]){ //this is if int1 has already been equaled to randNum1
             cout << "Error, you can only display one number at a time. Try again: " << endl << endl;
-            userChoice = false;
-        }
-        if(userChoice == false){ //asks user to decide again
             cout << int1 << "\t" << int2 << endl << endl;
             cout << "1. Display left number" << endl;
             cout << "2. Display right number" << endl;
@@ -365,14 +350,12 @@ int main() {
             cout << "5. Exit" << endl;
             cout << "What is your option?: ";
             break;
-            cin >> choice;
         }
 
-        else if (userChoice != false){ //if user choice does not equal false
+        if (userChoice != false){ //if user choice does not equal false
         
-        int1 = setdisplayLeft(hiddenArray, visArray, randnumArray, size, int1, int2);
+        int1 = setdisplayLeft(hiddenArray, visArray, randnumArray, size, int1, int2); //calls function to display lower bound
         break;
-        //cin >> choice;
 
         }
         break;
@@ -381,11 +364,8 @@ int main() {
         
         userChoice = true;
 
-        if(int1 == randnumArray[0]){ //if option 1 has already been chosen
+        if(int1 == randnumArray[0]){ //this is if int1 has already been equaled to randNum1
             cout << "Error, you can only display one number at a time. Try again: " << endl << endl;
-            userChoice = false;
-        }
-        if(userChoice == false){ //will make user choose again
             cout << int1 << "\t" << int2 << endl << endl;
             cout << "1. Display left number" << endl;
             cout << "2. Display right number" << endl;
@@ -394,14 +374,12 @@ int main() {
             cout << "5. Exit" << endl;
             cout << "What is your option?: ";
             break;
-            cin >> choice;
         }
 
-        else if (userChoice != false){ //does not equal false
+        if (userChoice != false){ //does not equal false
         
-        int2 = setdisplayRight(hiddenArray, visArray, randnumArray, size,  int1, int2);
+        int2 = setdisplayRight(hiddenArray, visArray, randnumArray, size,  int1, int2); //calls function to display upperbound
         break;
-        //cin >> choice;
         
         }
         
@@ -411,7 +389,15 @@ int main() {
         case 3:
 
         if(userPoints >= 0){
-        userPoints = funcGuess(userGuess, hiddenArray, visArray, userPoints, rowIndex, colIndex, int1, int2, randnumArray);
+        userPoints = funcGuess(userGuess, hiddenArray, visArray, userPoints, rowIndex, colIndex, int1, int2, randnumArray); //calls guess function then will update userPoints
+
+        if(userPoints < 0){ //ends the game if user points are less than 0
+                cout << endl << "Your total points balance: " << userPoints << endl;
+                cout << "You have run out of points." << endl;
+                cout << "Goodbye " << userName << "!" << endl;
+                userChoice = false; //ends game
+                break;
+            }
         
         isZero = allZeros(visArray, isZero);
 
@@ -422,15 +408,16 @@ int main() {
             cin >> choice2;
             if(choice2 == 'Y' || choice2 == 'y')
             {
-                randnumArray[size] = initialize(randnumArray, visArray, hiddenArray, size);
+                randnumArray[size] = initialize(randnumArray, visArray, hiddenArray, size); //restarts game
             }
             if(choice2 == 'N' || choice2 == 'n')
             {
+                userChoice = false;  //ends game
                 break;
             }
         }
         }
-        //userChoice = true;
+
         break;
 
         case 4:
@@ -438,9 +425,18 @@ int main() {
         userPoints -= 1; //doing this subtracts user's points by 1
 
         cout << "Game reset." << endl;
+
+            if(userPoints < 0){ //ends the game if user points are less than 0
+                cout << endl << "Your total points balance: " << userPoints << endl;
+                cout << "You have run out of points." << endl;
+                cout << "Goodbye " << userName << "!" << endl;
+                userChoice = false;
+                break;
+            }
+
         cout << "Your point balance: " << userPoints << endl; //shows balance after subtracted by 1
         
-        randnumArray[size] = initialize(randnumArray, visArray, hiddenArray, size);
+        randnumArray[size] = initialize(randnumArray, visArray, hiddenArray, size); //restarts game
         int1 = -1;
         int2 = -1;
         
@@ -457,18 +453,17 @@ int main() {
         case 5:
         cout << "Goodbye " << userName << "!" << endl;
         cout << "Your final points balance: " << userPoints << endl;
-        userChoice = true;
-        break; //ends game
+        userChoice = false; //ends game
+        break;
         
         default: //if user doesn't choose 1,2,3,4, or 5
             cout << "Error, invalid choice. Choose 1, 2, 3, 4, or 5: ";
             break;
         }
 
-        //}while(userChoice == false); //if it is false the loop will continue
-
     }while(userChoice == true); //if it is true the loop will end
     }
+    
 
     return 0;
 }
